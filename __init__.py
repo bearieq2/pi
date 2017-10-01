d45b82c7d34b9e19294c84698f78ca5b17736fe5
Martin Carroll <martin.carroll@nokia-bell-labs.com>
2017-10-01 11:57:51 -0400

    initial implementation
    Signed-off-by: Martin Carroll <martin.carroll@nokia-bell-labs.com>

---
+++ __init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+ 
+# Copyright (C) 2017 Alcatel-Lucent, Inc.
+# Authors:
+#   Martin Carroll <martin.carroll@nokia.com>
+#
+# confidential and proprietary
+# not for use or disclosure outside of nokia
+# or its affiliated companies except under written agreement
+
+#from __future__ import print_function
+
+__all__ = ['digits']
+
+def digits():
+    return '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+
+if __name__ == "__main__":
+    print(digits())
